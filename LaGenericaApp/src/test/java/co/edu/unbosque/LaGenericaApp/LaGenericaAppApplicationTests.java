3db549ec49c1a4283c76c9f9e901a949d9e8b516
Javcon02061984 <jcg_0206@hotmail.com>
2021-11-11 20:53:02 -0500

    actualizacion proyecto

---
+++ LaGenericaApp/src/test/java/co/edu/unbosque/LaGenericaApp/LaGenericaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.LaGenericaApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaGenericaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
